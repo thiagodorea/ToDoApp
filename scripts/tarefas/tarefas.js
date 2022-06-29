7c189b6e754aa069370e351d0e5c1d82a92d126b
thiagodorea <thiago.dorea@hotmail.com>
2022-06-29 20:30:36 -0300

    transforma função em async await

--- scripts/tarefas/tarefas.js
+++ scripts/tarefas/tarefas.js
@@ -3,6 +3,9 @@ let nomeUsuario = document.getElementById("nomeUsuario");
 let userImage = document.getElementById("userImage");
 let tarefasPendentes = document.getElementById("tarefasPendentes");
 
+const exampleEl = document.getElementById('example')
+const tooltip = new bootstrap.Tooltip(exampleEl, options)
+
 let tokenJwt;
 
 onload = function(){
@@ -18,7 +21,6 @@ sair.addEventListener("click",() =>{
 
 async function buscarInfoUsuario(){
     let configRequest = {
-        method: "GET",
         headers: {
             "Content-type":'Application/Json',
             "Authorization": `${tokenJwt}`
@@ -26,20 +28,18 @@ async function buscarInfoUsuario(){
     }
     try{
         let resp = await fetch(`${BASE_URL}/users/getMe`,configRequest)
-        let resposta = resp.json();
-        resposta.then(res => {
-            nomeUsuario.innerText = `Olá, ${res.firstName} ${res.lastName}`
-            userImage.style.backgroundImage ='url("../assets/face.png")';
-            buscarTask(res.id)
-        });
+        let resposta = await resp.json();
+        if(resp.status == 200){
+            setName(resposta);
+            buscarTask();
+        }
     } catch(error){
-        console.log(error);
+        showError(error);
     }
 }
 
-async function buscarTask(id){
+async function buscarTask(){
     let configRequest = {
-        method: "GET",
         headers: {
             "Content-type":'Application/Json',
             "Authorization": `${tokenJwt}`
@@ -47,36 +47,41 @@ async function buscarTask(id){
     }
     try{
         let resp = await fetch(`${BASE_URL}/tasks/`,configRequest)
-        let resposta = resp.json();
-        resposta.then(lista => {
-            if(lista.length == 0){
-                tarefasPendentes.innerHTML += `<div class="alert alert-primary" role="alert"> Você não tem nenhuma tarefa pendente. </div>`
-            }else{
-                for (item  of lista) {
-                    tarefasPendentes.innerHTML +=
-                    `
-                    <li class="tarefa">
-                    <div class="not-done"></div>
-                    <div class="descricao">
-                    <p class="nome">${item.description}</p>
-                    <p class="timestamp">Criada em: ${dataFormatada(item.createdAt)}</p>
-                    </div>
-                    </li>
-                    `
-                }
+        let resposta = await resp.json();
+        if(resposta.length == 0){
+            tarefasPendentes.innerHTML += `<div class="alert alert-primary" role="alert"> Você não tem nenhuma tarefa pendente. </div>`;
+        }else{
+            for (item  of resposta) {
+                setTask(item);
             }
-        });
+        }
     } catch(error){
-        console.log(error);
-    }
-}
+        showError(error);
+    };
+};
 
-function dataFormatada(dt){
-    var data = new Date(dt),
-        dia  = data.getDate().toString(),
-        diaF = (dia.length == 1) ? '0'+dia : dia,
-        mes  = (data.getMonth()+1).toString(), //+1 pois no getMonth Janeiro começa com zero.
-        mesF = (mes.length == 1) ? '0'+mes : mes,
-        anoF = data.getFullYear();
-    return diaF+"/"+mesF+"/"+anoF;
+function setName(dados){
+    nomeUsuario.innerText = `Olá, ${dados.firstName} ${dados.lastName}`;
+    userImage.style.backgroundImage ='url("../assets/face.png")';
+};
+
+function showError(msg){
+    tarefasPendentes.innerHTML += `<div class="alert alert-danger" role="alert"> Erro: ${msg} </div>`
+};
+
+function setTask(itenTask){
+    tarefasPendentes.innerHTML +=
+    `
+    <li class="tarefa">
+    <div class="not-done"></div>
+    <div class="descricao">
+    <p class="nome">${itenTask.description}</p>
+    <p class="timestamp">Criada em: ${dataFormatada(itenTask.createdAt)}</p>
+    </div>
+    </li>
+    `
+};
+
+function newTask(task){
+    console.log(task);
 }
--- scripts/validacoes/utils.js
+++ scripts/validacoes/utils.js
@@ -29,3 +29,13 @@ function validaCampo(campo,input,mensagem){
         input.style.border = formatoInvalido;
     }
 }
+
+function dataFormatada(dt){
+    var data = new Date(dt),
+        dia  = data.getDate().toString(),
+        diaF = (dia.length == 1) ? '0'+dia : dia,
+        mes  = (data.getMonth()+1).toString(), //+1 pois no getMonth Janeiro começa com zero.
+        mesF = (mes.length == 1) ? '0'+mes : mes,
+        anoF = data.getFullYear();
+    return diaF+"/"+mesF+"/"+anoF;
+}
