a1009d1aaa57f2c22ad05c3b71b6060771e489bf
thiagodorea <thiago.dorea@hotmail.com>
2022-07-05 10:30:08 -0300

    inseri função de excluir e atualizar a task

--- scripts/tarefas/tarefas.js
+++ scripts/tarefas/tarefas.js
@@ -8,6 +8,7 @@ let tarefasPendentes = document.getElementById("tarefasPendentes");
 let tarefasTerminadas = document.getElementById("tarefasTerminadas");
 let novaTarefa =document.getElementById("novaTarefa");
 let btnNewTask = document.getElementById("btnNewTask");
+let btnConcluido = document.getElementById("btnConcluido");
 
 let tokenJwt;
 let tarefa = {
@@ -27,18 +28,21 @@ sair.addEventListener("click",() =>{
 
 novaTarefa.addEventListener("keyup", () =>{
     validaTask(retiraEspacos(novaTarefa.value));
-})
+});
+
+btnConcluido.addEventListener("click",() =>{
+    btnConcluido.classList.toggle("done");
+    tarefa.completed == false ? tarefa.completed = true : tarefa.completed = false;
+});
 
 btnNewTask.addEventListener("click",(evento) =>{
     evento.preventDefault();
     if(validaTask(retiraEspacos(novaTarefa.value))){
-        tarefa.description = retiraEspacos(novaTarefa.value)
+        tarefa.description = retiraEspacos(novaTarefa.value);
         let tarefaJson = JSON.stringify(tarefa);
-        console.log(tarefaJson);
         enviarTarefa(tarefaJson);
-    }
-})
-
+    };
+});
 
 async function buscarInfoUsuario(){
     let configRequest = {
@@ -69,19 +73,13 @@ async function buscarTask(){
     try{
         let resp = await fetch(`${BASE_URL}/tasks/`,configRequest)
         let resposta = await resp.json();
+        // resposta.sort((x,y) => {return  y.id - x.id})
         resposta.map( task =>{
-                console.log(task)
-                // tarefasPendentes.innerHTML = '';
-                setTask(task);
-            });
+            setTask(task);
+        });
                 
             // if(resposta.length == 0){
             //     tarefasPendentes.innerHTML = `<div class="alert alert-primary" role="alert"> Você não tem nenhuma tarefa pendente. </div>`;
-            // }else{
-            //     tarefasPendentes.innerHTML = '';
-            //     for (item  of resposta) {
-            //         setTask(item);
-            //     }
             // }
         
     } catch(error){
@@ -91,6 +89,7 @@ async function buscarTask(){
 
 async function enviarTarefa(tarefaObj){
     btnSpinner();
+    retiraEspacos(tarefaObj)
     let configRequest = {
         method: "POST",
         headers: {
@@ -101,19 +100,95 @@ async function enviarTarefa(tarefaObj){
         body:tarefaObj
     } 
     try {
-        let resp = await fetch(`${BASE_URL}/tasks/`,configRequest)
+        let resp = await fetch(`${BASE_URL}/tasks/`,configRequest);
         let resposta = await resp.json();
         btnSpinner(resp.status);
         if (resp.status == 201 || resp.status == 200) {
+            tarefasPendentes.innerHTML = '';
+            tarefasTerminadas.innerHTML = '';
             novaTarefa.value = '';
-            validaTask(novaTarefa.value);
+            btnConcluido.classList.remove('done');
             buscarTask();
         } 
     } catch (error) {
-        btnSpinner(404);
+        btnSpinner(resp.status);
         toastAlert(error,"danger");
     }
-}
+};
+
+async function delTask(id){
+    let configRequest ={
+        method: "DELETE",
+        headers:{
+            "Content-type":'Application/Json',
+            "Access-Control-Allow-Origin": "*",
+            "Authorization": `${tokenJwt}`
+        }
+    };
+    try {
+        let resp = await fetch(`${BASE_URL}/tasks/${id}`,configRequest);
+        if(resp.status == 200){ 
+            tarefasPendentes.innerHTML = '';
+            tarefasTerminadas.innerHTML = '';
+            toastAlert("Atividade excluída.","success");
+            buscarTask();
+        }else if(resp.status == 400){
+            toastAlert("Id inválido.","danger");
+        }else if(resp.status == 401){
+            toastAlert("Você não tem autorização.","danger");
+        }else if(resp.status == 404){
+            toastAlert("Tarefa inexistente","danger");
+        }
+    } catch (error) {
+        toastAlert("Erro no Servidor, tente novamente","danger");
+    }
+};
+
+async function taskDone(id){
+    let configRequest= {
+        headers: {
+            "Content-type":'Application/Json',
+            "Authorization": `${tokenJwt}`
+        }
+    }
+    try{
+        let resp = await fetch(`${BASE_URL}/tasks/${id}`,configRequest)
+        let resposta = await resp.json();
+        if(resp.status == 200){
+            tarefasPendentes.innerHTML = '';
+            tarefasTerminadas.innerHTML = '';
+            resposta.completed == false ? tarefa.completed = true : tarefa.completed = false
+            tarefa.description = resposta.description;
+            let tarefaJson = JSON.stringify(tarefa);
+            let configRequest ={
+                method: "PUT",
+                headers:{
+                    "Content-type":'Application/Json',
+                    "Authorization": `${tokenJwt}`
+                },
+                body:tarefaJson
+            }
+            try {
+                let resp = await fetch(`${BASE_URL}/tasks/${id}`,configRequest);
+                let resposta = await resp.json();
+                if(resp.status == 200){ 
+                    resposta.completed == true ? toastAlert("Atividade concluida.","success") :toastAlert("Atividade aberta novamente.","success")
+                    buscarTask();
+                }else if(resp.status == 400){
+                    toastAlert("Id inválido.","danger");
+                }else if(resp.status == 401){
+                    toastAlert("Você não tem autorização.","danger");
+                }else if(resp.status == 404){
+                    toastAlert("Tarefa inexistente","danger");
+                }
+            } catch (error) {
+                toastAlert("Erro no Servidor, tente novamente","danger");
+            }
+        };
+    }catch(error){
+        toastAlert("Erro ao recuperar dados","danger");
+    };
+} ;
 
 function setName(dados){
     nomeUsuario.innerText = `Olá, ${dados.firstName} ${dados.lastName}`;
@@ -124,26 +199,30 @@ function setTask(itenTask){
     if(itenTask.completed == false){
         tarefasPendentes.innerHTML +=
         `
-        <li class="tarefa">
-            <div class="not-done"></div>
+        <li class="tarefa" id="tarefa ${itenTask.id}">
+            <div class="not-done" onclick="taskDone(${itenTask.id})"></div>
             <div class="descricao">
                 <p class="nome">${itenTask.description}</p>
-                <p class="timestamp">Criada em: ${dataFormatada(itenTask.createdAt)}</p>
+                <p class="timestamp">Criada em: ${dataFormatada(itenTask.createdAt)}
+                    <button class=" btn"><i class="fa-solid fa-pencil"></i><button>
+                </p>
             </div>
         </li>
         `
     }else{
         tarefasTerminadas.innerHTML +=
         `
-        <li class="tarefa">
-            <div class="not-done"></div>
+        <li class="tarefa" id="tarefa ${itenTask.id}">
+            <div class="not-done done" onclick="taskDone(${itenTask.id})"></div>
             <div class="descricao">
                 <p class="nome">${itenTask.description}</p>
-                <p class="timestamp">Criada em: ${dataFormatada(itenTask.createdAt)}</p>
+                <p class="timestamp">Criada em: ${dataFormatada(itenTask.createdAt)}
+                    <button class="btn" onclick="delTask(${itenTask.id})"><i class="fa-solid fa-trash"></i><button>
+                </p>
             </div>
         </li>
         `
-    }
+    };
 };
 
 function validaTask(task){
